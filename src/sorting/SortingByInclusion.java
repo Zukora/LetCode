c4a64fb177bed6d9e978ffbf42f689e742ab9a63
Oleg Androshchuk <oleg.androshchuk@gmail.com>
2020-06-03 09:40:45 +0300

    Inclusion sort

--- src/sorting/SortingByInclusion.java
+++ src/sorting/SortingByInclusion.java
@@ -2,16 +2,17 @@ package sorting;
 
 public class SortingByInclusion {
     private static int[] inclusionSort(int[] unsortArray) {
-        if (unsortArray.length < 2 ){
+        if (unsortArray.length < 2) {
             return unsortArray;
         }
-        for (int i=1; i < unsortArray.length ;i++) {
+        for (int i = 1; i < unsortArray.length; i++) {
             int key = unsortArray[i];
-            for (int j = i-1; j>=0;j--){
+            for (int j = i - 1; j >= 0; j--) {
                 if (key < unsortArray[j]) {
-                    swapElement(unsortArray,j,j+1);
+                    swapElement(unsortArray, j, j + 1);
                 }
             }
+            System.out.print(String.format("index = %s, key = %s, array after sort : ", i, key));
             printArray(unsortArray);
         }
         return unsortArray;
@@ -24,19 +25,24 @@ public class SortingByInclusion {
 
     }
 
-    private static void printArray (int[] array) {
-        for (int i=0; i < array.length ;i++) {
+    private static void printArray(int[] array) {
+        System.out.print("[");
+        for (int i = 0; i < array.length; i++) {
             System.out.print(array[i]);
+            if (i != array.length - 1) {
+                System.out.print(", ");
+            }
         }
-        System.out.println();
+        System.out.println("]");
     }
+
     public static void main(String[] args) {
         int[] myarray = new int[]{7, 3, 8, 4, 5, 6, 9, 1, 2};
         System.out.println("unsorted arrayy");
         printArray(myarray);
         System.out.println("Sorting steps...");
         System.out.println("--------------------");
-        printArray(inclusionSort(myarray));
+        inclusionSort(myarray);
         System.out.println("--------------------");
         System.out.println("Sort finish...");
     }
